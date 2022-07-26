78f292fe8e3eed3100414fc65a896b51588e5988
Tawanda <ejmunongo@gmail.com>
2022-07-26 14:03:09 +0200

    query and display country

--- src/index.js
+++ src/index.js
@@ -3,48 +3,48 @@ const errorMessage = document.getElementById('errorMessage')
 const selection = document.getElementById('selection')
 const results = document.getElementById('results')
 
-// country detail elements
-// create each element. assign it an id and a class name
-
-// const natName = document.createElement('span')
-// natName.id = 'aka'
-// const offName = document.createElement('p')
-// offName.id = 'official'
-// offName.className = 'card-text'
-// const flag = document.createElement('img')
-// flag.id = 'flag'
-// flag.className = 'card-img-top'
-// flag.alt = 'flag'
-// const cardBody = document.createElement('div')
-// cardBody.className = 'card-body'
-// const capital = document.createElement('p')
-// capital.id = 'capital'
-// capital.className = 'card-text'
-// const languages = document.createElement('p')
-// languages.id = 'languages'
-// languages.className = 'card-text'
-// const borders = document.createElement('p')
-// borders.id = 'borders'
-// borders.className = 'card-text'
-// const population = document.createElement('p')
-// population.id = 'population'
-// population.className = 'card-text'
-// const timezone = document.createElement('p')
-// timezone.id = 'timezone'
-// timezone.className = 'card-text'
-// const continent = document.createElement('p')
-// continent.id = 'continent'
-// continent.className = 'card-text'
-
 const pickCriteria = () => {
   el.placeholder = `Search by ${selection.value}`
 }
 
 const showCountry = (country) => {
   console.log('Data: ', country)
+
   const cName = document.createTextNode(`${country.name.common}`)
   cName.id = 'country-name'
   cName.className = 'card-title'
+  // country detail elements
+  // create each element. assign it an id and a class name
+
+  // const natName = document.createElement('span')
+  // natName.id = 'aka'
+  // const offName = document.createElement('p')
+  // offName.id = 'official'
+  // offName.className = 'card-text'
+  // const flag = document.createElement('img')
+  // flag.id = 'flag'
+  // flag.className = 'card-img-top'
+  // flag.alt = 'flag'
+  // const cardBody = document.createElement('div')
+  // cardBody.className = 'card-body'
+  // const capital = document.createElement('p')
+  // capital.id = 'capital'
+  // capital.className = 'card-text'
+  // const languages = document.createElement('p')
+  // languages.id = 'languages'
+  // languages.className = 'card-text'
+  // const borders = document.createElement('p')
+  // borders.id = 'borders'
+  // borders.className = 'card-text'
+  // const population = document.createElement('p')
+  // population.id = 'population'
+  // population.className = 'card-text'
+  // const timezone = document.createElement('p')
+  // timezone.id = 'timezone'
+  // timezone.className = 'card-text'
+  // const continent = document.createElement('p')
+  // continent.id = 'continent'
+  // continent.className = 'card-text'
 
   //create card div element inside results div
   const result = document.createElement('div')
